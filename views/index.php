cc391fc9b54b5785c8eaa0dbbae9bc8243b9254a
NEGROY <nerydiaz96@gmail.com>
2023-03-02 14:51:47 -0600

    YA FUNCIONA

--- views/index.php
+++ views/index.php
@@ -1,38 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="styles.css">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@600&display=swap">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-  
-  <script src="../include/js/wheel.js"> </script>
-  <link rel="stylesheet" href="../include/css/stilo.css">
-  <title>Spinner</title>
+  <title>Document</title>
 </head>
+  <!--  PARA la lllamda de funciones -->
+  <script type="text/javascript" src="../include/js/wheel.js"></script>
+
+  <script type="text/javascript">
+
+var options = {
+    value_set: [
+        {key:'Ganador',probability: 1, description: 'Ganador', color_img:'color-1.jpg',label_img:'label-1.png'},
+        {key:'Premio',probability: 1, description: 'Premio', color_img:'color-2.jpg',label_img:'label-2.png'},
+        {key:'HUUY',probability: 2, description: 'HUUY', color_img:'color-1.jpg',label_img:'label-3.png'},
+        {key:'Otra Vez',probability: 1, description: 'Otra Vez', color_img:'color-2.jpg',label_img:'label-4.png'},
+        {key:'Ganador',probability: 0, description: 'Ganador', color_img:'color-1.jpg',label_img:'label-5.png'},
+        {key:'Premio',probability: 0, description: 'Premio', color_img:'color-2.jpg',label_img:'label-6.png'}
+    ],
+    images: {
+        base_url: 'img/',
+        img_size: 400,
+        select_img: 'arrow.png'
+
+    },
+    result_callback: function(throw_nr,result_key){
+      alert('TIRADA No '+throw_nr+' Resultado '+result_key);
+    }
+};
+
+wheel_create_here('RUEDA RUEDA!',options);
+
+</script>
 <body>
-  <img id="hand" src="https://i.imgur.com/vJx9k4T.png">
-  <img id="wheel" src="https://i.imgur.com/MRyOcpy.png">
-  <div id="button">SPIN</div>
-  <div id="button2"onclick="toggle()">HISTORY</div>
 
-  <div id="history" class="hide"> <!--History Window-->
-    <div id="historytext"></div>  <!--The text inside that window injected by js-->
-  </div>
-
-  <div id="slot">YOU WON: <p id="text"></p></div> <!--Slot Prize Window-->
-
-  <div id="wallet"></div>
-  <div class="reducted"></div> <!--Animation-->
-
-  <ul class="info">
-    <li><img class="pic" src="https://i.imgur.com/UfHEbkR.png"> x0</li>
-    <li><img class="pic1" src="https://i.imgur.com/ka2cDlH.png"> x1.2</li>
-    <li><img class="pic2" src="https://i.imgur.com/7LnDOwj.png"> x2</li>
-    <li><img class="pic3" src="https://i.imgur.com/EOwnTfP.png">x5</li>
-  </ul>
-
-  <script src="js/script.js"></script>
+  BTN
+  
+  
 </body>
+</html>
